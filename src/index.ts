272795d664a29fe34e260fc82349dcfaac1a684c
Sasha Koss <koss@nocorp.me>
2024-02-07 15:05:00 +0800

    Implement TinyFloat

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,181 @@
-class TinyFloat {
-  int: BigInt;
-  presicion: number;
+/**
+ * A arbitrary-precision decimal class that uses BigInt to store the number.
+ * The implementation considers the presicion as the number of digits after
+ * decimal point to keep. It uses trunc method to round the number.
+ */
+export class TinyFloat {
+  /**
+   * The BigInt representation of the number. It's multiplied by 10^presicion.
+   *
+   * @private
+   */
+  private int: bigint;
 
-  constructor(ft: string, presicion: number = 9) {
+  /**
+   * The number of digits after decimal point to keep.
+   *
+   * @private
+   */
+  private presicion: number;
+
+  /**
+   * Creates a TinyFloat instance from a string and given presicion.
+   *
+   * @param str - The number string
+   * @param presicion - The number of digits after decimal point to keep
+   */
+  constructor(str?: string, presicion?: number) {
+    this.presicion = presicion ?? 9;
+    this.int = str ? TinyFloat.parse(str, this.presicion) : BigInt(0);
+  }
+
+  /**
+   * Returns the number as a string. It keeps the zero padding according to
+   * the presicion.
+   *
+   * @returns The number string.
+   */
+  toString(): string {
+    const str = (this.int < 0 ? -this.int : this.int).toString();
+    const point = str.length - this.presicion;
+    return (
+      (this.int < 0 ? "-" : "") +
+      (point > 0
+        ? str.slice(0, point) + "." + str.slice(point)
+        : "0." + "0".repeat(Math.abs(point)) + str.slice(0))
+    );
+  }
+
+  /**
+   * Returns the number type.
+   *
+   * @returns The number
+   */
+  toNumber(): number {
+    return parseFloat(this.toString());
+  }
+
+  /**
+   * Adds two numbers.
+   *
+   * @param tf - The number to add
+   *
+   * @returns Sum of the two numbers
+   */
+  add(tf: TinyFloat): TinyFloat {
+    const sum = new TinyFloat();
+    sum.set(this.int + tf.withPresicion(this.presicion).int, this.presicion);
+    return sum;
+  }
+
+  /**
+   * Subtracts two numbers.
+   *
+   * @param tf - The number to subtract
+   *
+   * @returns Difference of the two numbers
+   */
+  sub(tf: TinyFloat): TinyFloat {
+    const sub = new TinyFloat();
+    sub.set(this.int - tf.withPresicion(this.presicion).int, this.presicion);
+    return sub;
+  }
+
+  /**
+   * Multiplies two numbers.
+   *
+   * @param tf - The number to multiply
+   *
+   * @returns Product of the two numbers
+   */
+  mul(tf: TinyFloat): TinyFloat {
+    const mul = new TinyFloat();
+    mul.set(
+      (this.int * tf.withPresicion(this.presicion).int) /
+        BigInt(10 ** this.presicion),
+      this.presicion
+    );
+    return mul;
+  }
+
+  /**
+   * Divides two numbers.
+   *
+   * @param tf - The number to divide
+   *
+   * @returns Quotient of the two numbers
+   */
+  div(tf: TinyFloat): TinyFloat {
+    const div = new TinyFloat();
+    div.set(
+      (this.int * BigInt(10 ** this.presicion)) /
+        tf.withPresicion(this.presicion).int,
+      this.presicion
+    );
+    return div;
+  }
+
+  /**
+   * Returns the remainder of the division of two numbers.
+   *
+   * @param tf - The number to divide
+   *
+   * @returns The remainder of the division
+   */
+  mod(tf: TinyFloat): TinyFloat {
+    const mod = new TinyFloat();
+    mod.set(this.int % tf.withPresicion(this.presicion).int, this.presicion);
+    return mod;
+  }
+
+  /**
+   * Returns a new TinyFloat with the new presicion.
+   *
+   * @param presicion - The new presicion
+   *
+   * @returns A new TinyFloat with the new presicion
+   */
+  withPresicion(presicion: number): TinyFloat {
+    if (this.presicion === presicion) return this;
+    const tf = new TinyFloat();
+    const pow = BigInt(10 ** Math.abs(this.presicion - presicion));
+    const int = this.presicion > presicion ? this.int / pow : this.int * pow;
+    tf.set(int, presicion);
+    return tf;
+  }
+
+  /**
+   * Mutates the TinyFloat instance with the new BigInt and presicion.
+   *
+   * @param int - The BigInt to set
+   * @param presicion - The presicion to set
+   *
+   * @private
+   */
+  private set(int: bigint, presicion: number): void {
+    this.int = int;
     this.presicion = presicion;
+  }
 
-    const pointIndex = ft.indexOf(".");
-    if (pointIndex === -1) {
-      this.int = BigInt(ft + "0".repeat(presicion));
+  /**
+   * Parses the number string to BigInt.
+   *
+   * @param str - The number string
+   *
+   * @returns The BigInt representation of the number
+   *
+   * @private
+   */
+  private static parse(str: string, presicion: number): bigint {
+    const point = str.indexOf(".");
+    if (point === -1) {
+      return BigInt(str + "0".repeat(presicion));
     } else {
-      const intPart = ft.slice(0, pointIndex);
-      const floatPart = ft.slice(pointIndex + 1, pointIndex + 1 + presicion);
-      this.int = BigInt(
+      const intPart = str.slice(0, point);
+      const floatPart = str.slice(point + 1, point + 1 + presicion);
+      return BigInt(
         intPart + floatPart + "0".repeat(presicion - floatPart.length)
       );
     }
-  }
-
-  toNumber(): number {
-    // TODO: Make it right
-    return parseFloat(this.int.toString()) / 10 ** this.presicion;
   }
 }
---
+++ src/tests.ts
@@ -0,0 +1,247 @@
+import { describe, expect, it } from "vitest";
+import { TinyFloat } from ".";
+
+describe("TinyFloat", () => {
+  it("accepts whole numbers", () => {
+    expect(new TinyFloat("0").toNumber()).toBe(0);
+    expect(new TinyFloat("1").toNumber()).toBe(1);
+    expect(new TinyFloat("1234").toNumber()).toBe(1234);
+    expect(new TinyFloat("123456789").toNumber()).toBe(123456789);
+    expect(new TinyFloat("12345678901234567890").toNumber()).toBe(
+      12345678901234567890
+    );
+  });
+
+  it("accepts decimal numbers", () => {
+    expect(new TinyFloat("0.1").toNumber()).toBe(0.1);
+    expect(new TinyFloat("0.1234").toNumber()).toBe(0.1234);
+    expect(new TinyFloat("12.34").toNumber()).toBe(12.34);
+    expect(new TinyFloat("10.001234").toNumber()).toBe(10.001234);
+    expect(new TinyFloat("0.001234").toNumber()).toBe(0.001234);
+    expect(new TinyFloat("9876543210.123456789").toNumber()).toBe(
+      9876543210.123456789
+    );
+  });
+
+  it("allows to specify presicion", () => {
+    expect(new TinyFloat("0.123456789", 5).toNumber()).toBe(0.12345);
+    expect(new TinyFloat("0.123456789", 3).toNumber()).toBe(0.123);
+    expect(new TinyFloat("0.0001234", 4).toNumber()).toBe(0.0001);
+    expect(new TinyFloat("0.1234", 9).toNumber()).toBe(0.1234);
+    expect(new TinyFloat("0.1234", 9).toNumber()).toBe(0.1234);
+    expect(new TinyFloat("12.34", 1).toNumber()).toBe(12.3);
+    expect(new TinyFloat("10.12345678901234", 1).toNumber()).toBe(10.1);
+    expect(new TinyFloat("10.12345678901234", 12).toNumber()).toBe(
+      10.123456789012
+    );
+  });
+
+  it("accepts negative numbers", () => {
+    expect(new TinyFloat("-0.123456789", 5).toNumber()).toBe(-0.12345);
+    expect(new TinyFloat("-0.123456789", 3).toNumber()).toBe(-0.123);
+    expect(new TinyFloat("-0.123456789", 1).toNumber()).toBe(-0.1);
+    expect(new TinyFloat("-0.1", 3).toNumber()).toBe(-0.1);
+    expect(new TinyFloat("-1").toNumber()).toBe(-1);
+    expect(new TinyFloat("-1.2345", 3).toNumber()).toBe(-1.234);
+    expect(new TinyFloat("-321.123456789").toNumber()).toBe(-321.123456789);
+    expect(new TinyFloat("-321.123456789", 5).toNumber()).toBe(-321.12345);
+  });
+
+  describe("toString", () => {
+    it("returns the number as a string", () => {
+      expect(new TinyFloat("0").toString()).toBe("0.000000000");
+      expect(new TinyFloat("1").toString()).toBe("1.000000000");
+      expect(new TinyFloat("12345678901234567890").toString()).toBe(
+        "12345678901234567890.000000000"
+      );
+      expect(new TinyFloat("0.1234").toString()).toBe("0.123400000");
+      expect(new TinyFloat("12.34").toString()).toBe("12.340000000");
+
+      expect(new TinyFloat("0.1234", 6).toString()).toBe("0.123400");
+      expect(new TinyFloat("10.12345678901234", 12).toString()).toBe(
+        "10.123456789012"
+      );
+      expect(new TinyFloat("-0.123456789", 1).toString()).toBe("-0.1");
+    });
+  });
+
+  describe("toNumber", () => {
+    it("returns the number", () => {
+      expect(new TinyFloat("0").toNumber()).toBe(0.0);
+      expect(new TinyFloat("1").toNumber()).toBe(1.0);
+      expect(new TinyFloat("12345678901234567890").toNumber()).toBe(
+        12345678901234567890.0
+      );
+      expect(new TinyFloat("0.1234").toNumber()).toBe(0.1234);
+      expect(new TinyFloat("12.34").toNumber()).toBe(12.34);
+
+      expect(new TinyFloat("0.1234", 6).toNumber()).toBe(0.1234);
+      expect(new TinyFloat("10.12345678901234", 12).toNumber()).toBe(
+        10.123456789012
+      );
+      expect(new TinyFloat("-0.123456789", 1).toNumber()).toBe(-0.1);
+    });
+  });
+
+  describe("add", () => {
+    it("adds two numbers", () => {
+      expect(new TinyFloat("0.1").add(new TinyFloat("0.2")).toNumber()).toBe(
+        0.3
+      );
+      expect(new TinyFloat("-0.1").add(new TinyFloat("0.2")).toNumber()).toBe(
+        0.1
+      );
+      expect(new TinyFloat("0.1").add(new TinyFloat("-0.3")).toNumber()).toBe(
+        -0.2
+      );
+    });
+
+    it("uses the presicion of the first number", () => {
+      expect(
+        new TinyFloat("0.123456789", 1)
+          .add(new TinyFloat("0.123456789"))
+          .toNumber()
+      ).toBe(0.2);
+      expect(
+        new TinyFloat("0.123456789", 2)
+          .add(new TinyFloat("0.123456789"))
+          .toNumber()
+      ).toBe(0.24);
+      expect(
+        new TinyFloat("0.123456789", 2)
+          .add(new TinyFloat("-0.123456789", 1))
+          .toNumber()
+      ).toBe(0.02);
+    });
+  });
+
+  describe("sub", () => {
+    it("subtracts two numbers", () => {
+      expect(new TinyFloat("0.1").sub(new TinyFloat("0.2")).toNumber()).toBe(
+        -0.1
+      );
+      expect(new TinyFloat("-0.1").sub(new TinyFloat("0.2")).toNumber()).toBe(
+        -0.3
+      );
+      expect(new TinyFloat("0.1").sub(new TinyFloat("-0.3")).toNumber()).toBe(
+        0.4
+      );
+    });
+
+    it("uses the presicion of the first number", () => {
+      expect(
+        new TinyFloat("0.123456789", 1).sub(new TinyFloat("0.19")).toNumber()
+      ).toBe(0);
+      expect(
+        new TinyFloat("0.123456789", 2).sub(new TinyFloat("0.19")).toNumber()
+      ).toBe(-0.07);
+      expect(
+        new TinyFloat("0.123456789", 2)
+          .sub(new TinyFloat("-0.123456789", 1))
+          .toNumber()
+      ).toBe(0.22);
+    });
+  });
+
+  describe("mul", () => {
+    it("multiplies two numbers", () => {
+      expect(new TinyFloat("6").mul(new TinyFloat("2")).toNumber()).toBe(12);
+      expect(new TinyFloat("0.6").mul(new TinyFloat("0.2")).toNumber()).toBe(
+        0.12
+      );
+      expect(new TinyFloat("0.06").mul(new TinyFloat("0.02")).toNumber()).toBe(
+        0.0012
+      );
+      expect(
+        new TinyFloat("6.17283945").mul(new TinyFloat("0.2")).toNumber()
+      ).toBe(1.23456789);
+      expect(new TinyFloat("6.2").mul(new TinyFloat("-2.1")).toNumber()).toBe(
+        -13.02
+      );
+    });
+
+    it("uses the presicion of the first number", () => {
+      expect(
+        new TinyFloat("0.123456789", 3)
+          .mul(new TinyFloat("0.123456789"))
+          .toNumber()
+      ).toBe(0.015);
+      expect(
+        new TinyFloat("0.123456789", 3)
+          .mul(new TinyFloat("-0.123456789", 1))
+          .toNumber()
+      ).toBe(-0.012);
+    });
+  });
+
+  describe("div", () => {
+    it("divides two numbers", () => {
+      expect(new TinyFloat("6").div(new TinyFloat("2")).toNumber()).toBe(3);
+      expect(
+        new TinyFloat("1.23456789").div(new TinyFloat("0.2")).toNumber()
+      ).toBe(6.17283945);
+      expect(new TinyFloat("6.2").div(new TinyFloat("-2.1")).toNumber()).toBe(
+        -2.952380952
+      );
+    });
+
+    it("uses the presicion of the first number", () => {
+      expect(
+        new TinyFloat("0.123456789", 3)
+          .div(new TinyFloat("0.123456789"))
+          .toNumber()
+      ).toBe(1);
+      expect(
+        new TinyFloat("0.123456789", 3)
+          .div(new TinyFloat("-0.123456789", 1))
+          .toNumber()
+      ).toBe(-1.23);
+    });
+  });
+
+  describe("mod", () => {
+    it("returns the remainder of the division", () => {
+      expect(new TinyFloat("6").mod(new TinyFloat("2")).toNumber()).toBe(0);
+      expect(new TinyFloat("6.6").mod(new TinyFloat("2")).toNumber()).toBe(0.6);
+      expect(new TinyFloat("0.6").mod(new TinyFloat("2")).toNumber()).toBe(0.6);
+      expect(new TinyFloat("6").mod(new TinyFloat("0.02")).toNumber()).toBe(0);
+      expect(new TinyFloat("6.003").mod(new TinyFloat("0.02")).toNumber()).toBe(
+        0.003
+      );
+      expect(new TinyFloat("6").mod(new TinyFloat("4")).toNumber()).toBe(2);
+      expect(new TinyFloat("6").mod(new TinyFloat("5")).toNumber()).toBe(1);
+      expect(new TinyFloat("6").mod(new TinyFloat("3.5")).toNumber()).toBe(2.5);
+      expect(new TinyFloat("-6").mod(new TinyFloat("3.1")).toNumber()).toBe(
+        -2.9
+      );
+      expect(new TinyFloat("6").mod(new TinyFloat("3")).toNumber()).toBe(0);
+    });
+
+    it("uses the presicion of the first number", () => {
+      expect(
+        new TinyFloat("0.123456789", 3)
+          .mod(new TinyFloat("0.123456789"))
+          .toNumber()
+      ).toBe(0);
+      expect(
+        new TinyFloat("0.123456789", 3)
+          .mod(new TinyFloat("-0.123456789", 1))
+          .toNumber()
+      ).toBe(0.023);
+    });
+  });
+
+  describe("withPresicion", () => {
+    it("changes the presicion", () => {
+      expect(new TinyFloat("0.123456789", 5).withPresicion(3).toNumber()).toBe(
+        0.123
+      );
+      expect(new TinyFloat("0.123456789", 2).withPresicion(5).toNumber()).toBe(
+        0.12
+      );
+      expect(new TinyFloat("-0.123456789", 2).withPresicion(5).toNumber()).toBe(
+        -0.12
+      );
+    });
+  });
+});
