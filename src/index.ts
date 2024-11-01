bb64b185a2e8205fba0e50c94652ca92c387127f
raduflp <radufilip2@gmail.com>
2024-11-01 15:01:40 +0100

    fix: don't throw errors when parsing small numbers

--- src/index.ts
+++ src/index.ts
@@ -184,7 +184,13 @@ export class TinyFloat {
    * @private
    */
   private parse(num: string | number): bigint {
-    const str = num.toString();
+    let str = num.toString();
+    // numbers less that 0.000001 are converted to exponential notation, we want to keep the dot notation
+    if (typeof num === "number" && num < 0.000001)
+      str = num.toLocaleString("en-US", {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: this.precision,
+      });
     const digits = this.precision + 1;
     const point = str.indexOf(".");
     if (point === -1) {
