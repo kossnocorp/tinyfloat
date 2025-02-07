83cee7c57177d4582a76cb03589f45f18e965fc8
Sasha Koss <koss@nocorp.me>
2025-02-07 09:51:21 +0800

    Set up dev env with Nix

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "TinyFloat dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              # Tools
+              direnv
+              # Node.js
+              nodejs
+              corepack
+              # GitHub
+              gh
+            ];
+          };
+      }
+    );
+}
